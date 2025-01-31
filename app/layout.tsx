dc5ef198000e03f02d512ad310afc08f722ea784
William BASTARD <65223866+williambastard@users.noreply.github.com>
2025-01-31 21:59:34 +0100

    📝 mise à jour(layout.tsx) : corriger une faute de frappe dans la description La description du site a été corrigée pour supprimer une répétition de texte. Cela améliore la lisibilité et la cohérence du contenu affiché aux utilisateurs.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/app/components/theme-provider";
 export const metadata: Metadata = {
   title: "Mirella Pietra : Boutique de bijoux artisanaux et de lithothérapie",
   description:
-    "Bijoux artisanaux en pierres naturelles et lithothérapie. Créations uniques faites main en France, alliant l'esthétique à l'énergie des pierres. Trouvez le bijou qui vous correspond parmi nos colliers, bracelets, bagues et boucles d'oreilles en Améthyste, Quartz Rose, Oeil de Tigre et bien d'autres.  Explorez les vertus des minéraux et révélez votre éclat intérieur. Livraison rapide et soignée.",
+    "Bijoux artisanaux en pierres naturelles et lithothérapie. Créations uniques faites main en France, alliant l'esthétique à l'énergie des pierres. Trouvez le bijou qui vous correspond parmi nos colliers, bracelets, bagues et boucles d'oreilles en Améthyste, Quartz Rose, Oeil de Tigre et bien d'autres. Explorez les vertus des minéraux et révélez votre éclat intérieur. Livraison rapide et soignée.",
 };
 
 export default function RootLayout({
