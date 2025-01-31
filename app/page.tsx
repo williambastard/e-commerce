decd4279ec529a8799b3a2f569a1f90fc6c16ea0
William BASTARD <65223866+williambastard@users.noreply.github.com>
2025-01-31 19:45:06 +0100

    🔧 config(.prettierrc) : ajouter la configuration Prettier avec des plugins TailwindCSS ✨ fonctionnalité(app/page.tsx) : ajouter des composants de barre de navigation et de miettes de pain ✅ tests(app/tests/page.tsx) : ajouter des tests pour le composant de page La configuration Prettier a été ajoutée pour inclure des plugins TailwindCSS et spécifier les fichiers de styles et de configuration. De plus, des composants de barre de navigation et de miettes de pain ont été ajoutés à la page, améliorant ainsi la navigation et la structure visuelle de l'application. Enfin, des tests ont été ajoutés pour le composant de page, garantissant son bon fonctionnement et sa fiabilité.
    📝 mettre à jour(globals.css) : ajouter de nouvelles couleurs personnalisées et les renommer en français
    Les couleurs personnalisées ont été ajoutées et renommées en français pour améliorer la lisibilité et la compréhension du code. Cela rend le code plus convivial pour les développeurs francophones et facilite la maintenance du style.
    
    📝 mettre à jour(globals.css) : ajouter de nouvelles variables de couleur et les commenter en français
    Les nouvelles variables de couleur ont été ajoutées pour améliorer la lisibilité et la maintenabilité du code CSS. Les commentaires en français ont été inclus pour expliquer la signification de chaque couleur, facilitant ainsi la compréhension du code pour les développeurs francophones.
    
    ✨ ajouter(components/tests/product.tsx, components/tests/productSkeleton.tsx, components/ui/hero.tsx, components/ui/product-buy-form.tsx) : introduire de nouveaux composants et fonctionnalités
    Les fichiers ajoutés incluent un composant de carte de produit, un squelette de produit, un composant de héros et un formulaire d'achat de produit. Ces ajouts enrichissent l'expérience utilisateur en permettant l'affichage de produits, la visualisation de contenus de manière dynamique et la possibilité d'acheter des produits directement depuis l'interface.
    
    ✨ ajouter(ui/product-list-thumbnail.tsx, ui/product-list.tsx, data/productInterface.tsx, data/products.tsx, lib/actions.ts, lib/products.ts, lib/schema.ts) : créer de nouveaux composants, interfaces et fonctions pour afficher une liste de produits
    Ces modifications ajoutent de nouveaux fichiers et fonctionnalités pour afficher une liste de produits dans l'application. Les nouveaux composants et interfaces permettent de présenter les produits de manière structurée et attrayante, tandis que les nouvelles fonctions facilitent la gestion des produits et des actions liées à l'achat.
    
    ✨ ajouter(stripe.ts) : créer un nouveau fichier pour gérer les interactions avec l'API Stripe, y compris la création de sessions de paiement
    🔧 config(next.config.ts) : ajouter un nouveau domaine autorisé pour les images provenant de files.stripe.com
    ⬆️ dépendances(package.json) : mettre à jour la version de la bibliothèque Stripe à 17.6.0
    📝 ajouter(products.json) : créer un fichier JSON contenant des informations sur des produits de test
    Les modifications apportées incluent l'ajout d'un fichier pour gérer les interactions avec l'API Stripe, la configuration d'un nouveau domaine autorisé pour les images, la mise à jour de la version de la bibliothèque Stripe, et la création d'un fichier JSON pour stocker des informations sur des produits de test.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,57 @@
+import { AppSidebar } from "@/components/sink/app-sidebar";
+import { NavigationMenuDemo } from "@/components/sink/navigation-menu-demo";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
+
 export default function Home() {
-	return <></>;
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarInset>
+				<header className="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+					<div className="flex items-center gap-2 px-4 w-full">
+						<SidebarTrigger className="-ml-1" />
+						<Separator
+							orientation="vertical"
+							className="mr-2 h-4"
+						/>
+						<NavigationMenuDemo />
+					</div>
+				</header>
+				<div className="hidden md:block">
+					<div className="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+						<Breadcrumb className="flex items-center gap-2 px-4 w-full">
+							<BreadcrumbList>
+								<BreadcrumbItem>
+									<BreadcrumbLink href="#">
+										Building Your Application
+									</BreadcrumbLink>
+								</BreadcrumbItem>
+								<BreadcrumbSeparator />
+								<BreadcrumbItem>
+									<BreadcrumbPage>
+										Data Fetching
+									</BreadcrumbPage>
+								</BreadcrumbItem>
+							</BreadcrumbList>
+						</Breadcrumb>
+					</div>
+				</div>
+
+				<main className="flex flex-1 flex-col gap-4 p-4"></main>
+			</SidebarInset>
+		</SidebarProvider>
+	);
 }
